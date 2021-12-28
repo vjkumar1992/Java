6e44d816111bcec19090b32ed10c5f8019676757
vjkumar1992 <88926929+vjkumar1992@users.noreply.github.com>
2021-12-28 12:10:08 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,12 @@
+class Car
+{
+public static void main (String[] args)
+{
+int car_cost=  1500000;
+String car_model=  "Fortuner";
+String car_type=  "Diesel";
+System.out.println("car cost is"  +car_cost);
+System.out.println("car model is"  +car_model);
+System.out.println("car type is"  +car_type);
+}
+}
